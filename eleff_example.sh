13ae9a2ac4f4a586c73e46b3b723c3d1e930353f
Administrator <admin@example.com>
2017-06-26 12:57:31 +0000

    commit 2

--- eleff_example.sh
+++ eleff_example.sh
@@ -8,21 +8,21 @@ echo "Enter minor version"
 read minor
 
 version=$major.$minor
-branch_name="release/$version"
+branch_name="$version"
 
 
 echo "Checking for branch $branch_name"
 
-if [ `git branch --list $branch_name `]
+if  git ls-remote https://github.com/deleff/$branch_name ;
 then
    echo "Branch name $branch_name already exists."
 
-   tag=`git describe --tags`
-   echo "last tag was $tag"
+#   tag=`git describe --tags`
+   echo "last tag was `git describe --abbrev=0 --tags ` "
 
 else
    echo "creating branch $branch_name"
-   git checkout -b $branch_name
+#   git checkout -b $branch_name
 fi
 
 #git push -u origin $branch_name
